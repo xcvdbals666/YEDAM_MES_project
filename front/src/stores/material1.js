6f8a35679fd859fb584c32c62103cb2ddcc6dbc5
xcvdbals666 <xcvdbals666@naver.com>
2026-01-19 23:33:44 +0900

    HYM_260119_자재구매요청_작성자 모달 및 검색 추가

--- front/src/stores/material1.js
+++ front/src/stores/material1.js
@@ -3,18 +3,9 @@ import axios from 'axios';
 
 export const useMaterialStore = defineStore('material', {
   // state
-  state: () => ({
-    employees: []
-  }),
+  state: () => ({}),
   // getters
   // actions
-  actions: {
-    // 작성자 정보 불러오기
-    async fetchEmployees() {
-      const response = await axios.get(`/material/emp`);
-      console.log(response.data);
-      this.employees = response.data;
-    }
-  },
+  actions: {},
   persist: true
 });
