5bcfafb599da2e418187bfeb507de0db4ef08422
Junkwood <156729867+Junkwood@users.noreply.github.com>
2026-01-19 17:08:20 +0900

    SJW2026-01-19주문서관리페이지제작

--- back/database/sqls/order.js
+++ back/database/sqls/order.js
@@ -1,12 +1,13 @@
 // 전체조회
-const selectAll = `SELECT board_id
-        , title
-        , content
-        , author
-        , create_date
-FROM board
+const selectAllOrder = `SELECT *
+FROM ord_tbl
 ORDER BY board_id`;
 
+// 주문작성시 client 목록 가져오기(납품업체)
+const selectAllClient = `SELECT client_code,client_name 
+FROM client_tbl 
+WHERE client_type = 'l1';`;
 module.exports = {
-  selectAll,
+  selectAllOrder,
+  selectAllClient,
 };
--- back/routers/orderRouter.js
+++ back/routers/orderRouter.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const orderServie = require("../services/orderServie.js");
 
 router.get(`/order`, async (req, res) => {
-  let list = await orderServie.findAll();
+  let list = await orderServie.findAllOrder();
   res.send(list);
 });
 
+router.get(`/clientList`, async (req, res) => {
+  let list = await orderServie.findAllClient();
+  res.send(list);
+});
 module.exports = router;
