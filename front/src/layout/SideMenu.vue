450808ae969a19478f6667c735bfac0669fee3d2
oseongnuna <7040101a@gmail.com>
2026-01-19 18:12:23 +0900

    NJH_11

--- front/src/layout/SideMenu.vue
+++ front/src/layout/SideMenu.vue
@@ -3,135 +3,135 @@ import { ref } from 'vue';
 import SideMenuItem from './SideMenuItem.vue';
 
 const model = ref([
-  {
-    label: 'MES',
-    icon: 'pi pi-fw pi-briefcase',
-    path: '/pages',
-    items: [
-      {
-        label: '테스트화면(후에 대시보드 메인화면으로 교체)',
-        icon: 'pi pi-fw pi-user',
-        path: '/test'
-      },
-      {
-        label: '기준정보',
-        icon: 'pi pi-fw pi-user',
-        path: '/info',
+    {
+        label: 'MES',
+        icon: 'pi pi-fw pi-briefcase',
+        path: '/pages',
         items: [
-          {
-            label: '예시 메뉴',
-            icon: 'pi pi-fw pi-sign-in',
-            to: ''
-          }
+            {
+                label: '테스트화면(후에 대시보드 메인화면으로 교체)',
+                icon: 'pi pi-fw pi-user',
+                path: '/test'
+            },
+            {
+                label: '기준정보',
+                icon: 'pi pi-fw pi-user',
+                path: '/info',
+                items: [
+                    {
+                        label: '예시 메뉴',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: ''
+                    }
+                ]
+            },
+            {
+                label: '주문',
+                icon: 'pi pi-fw pi-user',
+                path: '/order',
+                items: [
+                    {
+                        label: '출고 조회',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: '/outbound'
+                    }
+                ]
+            },
+            {
+                label: '생산',
+                icon: 'pi pi-fw pi-user',
+                path: '/produce',
+                items: [
+                    {
+                        label: '생산계획관리',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: '/production/plan'
+                    },
+                    {
+                        label: '생산계획조회',
+                        icon: 'pi pi-fw pi-times-circle',
+                        to: '/production/planList'
+                    },
+                    {
+                        label: 'MRP관리',
+                        icon: 'pi pi-fw pi-lock',
+                        to: ''
+                    },
+                    {
+                        label: 'MRP조회',
+                        icon: 'pi pi-fw pi-lock',
+                        to: ''
+                    },
+                    {
+                        label: '작업지시서 관리',
+                        icon: 'pi pi-fw pi-lock',
+                        to: ''
+                    },
+                    {
+                        label: '작업지시서 조회',
+                        icon: 'pi pi-fw pi-lock',
+                        to: ''
+                    },
+                    {
+                        label: '작업진행 목록',
+                        icon: 'pi pi-fw pi-lock',
+                        to: ''
+                    },
+                    {
+                        label: '생산실적조회',
+                        icon: 'pi pi-fw pi-lock',
+                        to: '/production/result'
+                    }
+                ]
+            },
+            {
+                label: '품질',
+                icon: 'pi pi-fw pi-user',
+                path: '/quality',
+                items: [
+                    {
+                        label: '예시 메뉴',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: ''
+                    }
+                ]
+            },
+            {
+                label: '자재',
+                icon: 'pi pi-fw pi-user',
+                path: '/material',
+                items: [
+                    {
+                        label: '예시 메뉴',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: ''
+                    }
+                ]
+            },
+            {
+                label: '설비',
+                icon: 'pi pi-fw pi-user',
+                path: '/facility',
+                items: [
+                    {
+                        label: '예시 메뉴',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: ''
+                    }
+                ]
+            }
         ]
-      },
-      {
-        label: '주문',
-        icon: 'pi pi-fw pi-user',
-        path: '/order',
-        items: [
-          {
-            label: '예시 메뉴',
-            icon: 'pi pi-fw pi-sign-in',
-            to: ''
-          }
-        ]
-      },
-      {
-        label: '생산',
-        icon: 'pi pi-fw pi-user',
-        path: '/produce',
-        items: [
-          {
-            label: '생산계획관리',
-            icon: 'pi pi-fw pi-sign-in',
-            to: '/production/plan'
-          },
-          {
-            label: '생산계획조회',
-            icon: 'pi pi-fw pi-times-circle',
-            to: '/production/planList'
-          },
-          {
-            label: 'MRP관리',
-            icon: 'pi pi-fw pi-lock',
-            to: ''
-          },
-          {
-            label: 'MRP조회',
-            icon: 'pi pi-fw pi-lock',
-            to: ''
-          },
-          {
-            label: '작업지시서 관리',
-            icon: 'pi pi-fw pi-lock',
-            to: ''
-          },
-          {
-            label: '작업지시서 조회',
-            icon: 'pi pi-fw pi-lock',
-            to: ''
-          },
-          {
-            label: '작업진행 목록',
-            icon: 'pi pi-fw pi-lock',
-            to: ''
-          },
-          {
-            label: '생산실적조회',
-            icon: 'pi pi-fw pi-lock',
-            to: '/production/result'
-          }
-        ]
-      },
-      {
-        label: '품질',
-        icon: 'pi pi-fw pi-user',
-        path: '/quality',
-        items: [
-          {
-            label: '예시 메뉴',
-            icon: 'pi pi-fw pi-sign-in',
-            to: ''
-          }
-        ]
-      },
-      {
-        label: '자재',
-        icon: 'pi pi-fw pi-user',
-        path: '/material',
-        items: [
-          {
-            label: '예시 메뉴',
-            icon: 'pi pi-fw pi-sign-in',
-            to: ''
-          }
-        ]
-      },
-      {
-        label: '설비',
-        icon: 'pi pi-fw pi-user',
-        path: '/facility',
-        items: [
-          {
-            label: '예시 메뉴',
-            icon: 'pi pi-fw pi-sign-in',
-            to: ''
-          }
-        ]
-      }
-    ]
-  }
+    }
 ]);
 </script>
 
 <template>
-  <ul class="layout-menu">
-    <template v-for="(item, i) in model" :key="item">
-      <side-menu-item v-if="!item.separator" :item="item" :index="i"></side-menu-item>
-      <li v-if="item.separator" class="menu-separator"></li>
-    </template>
-  </ul>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <side-menu-item v-if="!item.separator" :item="item" :index="i"></side-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ front/src/router/order2.js
@@ -0,0 +1,9 @@
+const Order2 = [
+    {
+        path: '/outbound',
+        name: 'outbound',
+        component: () => import('@/views/order/OutboundList.vue')
+    }
+];
+
+export default Order2;
