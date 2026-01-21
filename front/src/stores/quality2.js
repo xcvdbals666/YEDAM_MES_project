2e8b702beba60855136f96f88d59cb55027dd7a8
note981202-stack <note981202@gmail.com>
2026-01-21 17:08:34 +0900

    KHW_2026.01.21_지시조회창 모달, 보수작업

--- front/src/stores/quality2.js
+++ front/src/stores/quality2.js
@@ -2,7 +2,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-export const useQualityStore = defineStore('quality2', {
+export const useQualityStore2 = defineStore('quality2', {
   // state
   state: () => ({
     qiOrderList: []
@@ -12,9 +12,22 @@ export const useQualityStore = defineStore('quality2', {
 
   //action
   actions: {
+    // 품질검사 목록조회
     async fetchQiOrderList({ keyword }) {
       const res = await axios.get(`/api/quality/qi-order`, { params: keyword });
       this.qiOrderList = res.data;
+    },
+
+    // 검사지시서 전체 불러오기
+    async fetchOrderList() {
+      const response = await axios.get('/api/quality/qiorderlist');
+      response.data.forEach((data) => {
+        let date = new Date(data.qio_date);
+        data.qio_date = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      });
+      this.qiOrderList = response.data;
+      console.log('qiOrderList: ', this.qiOrderList);
+      return this.qiOrderList;
     }
   },
   persist: true
