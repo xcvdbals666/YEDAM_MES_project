5ef048e8727dc34f371df14266cbebcad33bb035
mocha2855 <mocha2855@gmail.com>
2026-01-23 02:56:26 +0900

    JDJ_0123_ㅠㅠ

--- back/database/sqls/produce1.js
+++ back/database/sqls/produce1.js
@@ -82,6 +82,129 @@ SET
 WHERE wko_code = ?
 `;
 
+//작업진행 단건 상세조회
+const selectWipDetail = `
+SELECT
+  w.wko_code,
+  w.wko_name,
+  w.wko_qtt,
+  w.line_code,
+  w.stat,
+  w.reg_date,
+  w.prod_code,
+  p.prod_name,
+  r.prdr_code
+FROM wko_tbl w
+JOIN prod_tbl p
+  ON p.prod_code = w.prod_code
+LEFT JOIN prdr_tbl r
+  ON r.work_order_code = w.wko_code 
+WHERE w.wko_code = ?
+LIMIT 1
+`;
+
+//라인 기준 사용해야 할 설비 목록 뽑아오기
+//ex. LINE-001에 매핑된 eq_code전부의 eq_name 띄우기
+const selectEqnameByLine = `
+SELECT
+  ld.line_eq_code, 
+  ld.line_code,
+  ld.eq_code,
+  e.eq_name,
+  e.eq_type
+FROM line_d_tbl ld
+JOIN eq_tbl e
+  ON e.eq_code = ld.eq_code
+WHERE ld.line_code = ?     
+ORDER BY ld.eq_code
+`;
+
+//선택한 wko_tbl의 prod_code로 타고가서 po_tbl에서 공정명 드롭다운 뽑기
+const selectProcessDropdownByWko = `
+    SELECT DISTINCT
+      d.po_code,
+      po.po_name,
+      d.no,
+      d.eq_type
+    FROM wko_tbl w
+    JOIN prod_proc_tbl pp
+      ON pp.prod_code = w.prod_code
+    JOIN prod_proc_d_tbl d
+      ON d.prod_proc_code = pp.prod_proc_code
+    JOIN po_tbl po
+      ON po.po_code = d.po_code
+    WHERE w.wko_code = ?
+    ORDER BY d.no
+  `;
+
+//작업시작 버튼 누르면 필요한 쿼리
+//prdr 테이블 번호생성
+const selectNextPrdrSeq = `
+SELECT IFNULL(
+  MAX(CAST(SUBSTRING(prdr_code, 6) AS UNSIGNED)), 0
+) AS maxSeq
+FROM prdr_tbl
+`;
+
+//prdr_d 테이블 번호생성
+const selectNextPrdrDSeq = `
+SELECT IFNULL(
+  MAX(CAST(SUBSTRING(prdr_d_code, 8) AS UNSIGNED)), 0
+) AS maxSeq
+FROM prdr_d_tbl;
+`;
+
+//prdr_tbl
+const insertPrdrStart = `
+INSERT INTO prdr_tbl
+  (prdr_code, prod_code, start_date, work_order_code, stat, ord_qtt, emp_code)
+VALUES
+  (?, ?, NOW(), ?, 'b1', ?, 'EMP-10001')
+`;
+
+//prdr_d_tbl
+const insertPrdrDStart = `
+INSERT INTO prdr_d_tbl
+  (prdr_d_code, prdr_code, input_qtt, start_date, line_eq_code)
+VALUES
+  (?, ?, ?, NOW(), ?)
+`;
+
+//#####제정신아님. . .
+// 특정 작업지시서의 설비별 생산실적 조회
+const selectPrdrStatusByWko = `
+SELECT
+  ld.line_eq_code,
+  e.eq_name,
+  pd.prdr_d_code,
+  pd.start_date,
+  pd.end_date
+FROM wko_tbl w
+JOIN prdr_tbl r
+  ON r.work_order_code = w.wko_code
+JOIN prdr_d_tbl pd
+  ON pd.prdr_code = r.prdr_code
+RIGHT JOIN line_d_tbl ld
+  ON ld.line_eq_code = pd.line_eq_code
+JOIN eq_tbl e
+  ON e.eq_code = ld.eq_code
+WHERE w.wko_code = ?
+ORDER BY ld.line_eq_code
+`;
+
+// 특정 prdr_d 상세 조회
+const selectPrdrDDetail = `
+SELECT
+  pd.prdr_d_code,
+  pd.prdr_code,
+  pd.input_qtt,
+  pd.start_date,
+  pd.end_date,
+  pd.line_eq_code
+FROM prdr_d_tbl pd
+WHERE pd.prdr_d_code = ?
+`;
+
 module.exports = {
   selectAllLinesDJ,
   selectPrdpActive,
@@ -91,4 +214,13 @@ module.exports = {
   deleteWorkOrder,
   existsWorkOrder,
   updateWorkOrder,
+  selectWipDetail,
+  selectEqnameByLine,
+  selectProcessDropdownByWko,
+  selectNextPrdrSeq,
+  selectNextPrdrDSeq,
+  insertPrdrStart,
+  insertPrdrDStart,
+  selectPrdrStatusByWko,
+  selectPrdrDDetail,
 };
